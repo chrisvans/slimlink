8c281607eb9d63864925fda216c5cf537d673a4a
chrisvans <chrisvanschyndel@gmail.com>
2013-09-11 12:06:13 -0400

    Building out validation, helper functions

--- slimlink/index.php
+++ slimlink/index.php
@@ -1,57 +1,7 @@
 <?php
-  $error_message = '';
-  $success_message = '';
-  $information_message = '';
 
-  $dbhost = "localhost";
-  $dbuser = "chris_php";
-  $dbpass = "bagel";
-  $dbname = "php_chris";
-  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-  if(mysqli_connect_error()) {
-    die(
-        mysqli_connect_error() . " (" .
-        mysqli_connect_errno() . ")"
-       );
-      }
-  // Logic to check against POST, valid POST, generate 6 character string.
-  // Validate url
-  // Check for 'http' in url
-  // Remove http:// or https:// from url for generic DB save
-  // Check to see if the URL already exists in the DB
-
-  function is_valid_url($url) {
-    return (filter_var($url, FILTER_VALIDATE_URL) !== false);
-  }
-
-  if (isset($_POST["url"])) {
-    print_r($_POST); echo '<br />';
-    $url = $_POST["url"];
-    $url = add_http_url($url);
-    if (is_valid_url($url) === True) {
-      $url = remove_http_url($url);
-      $query = "SELECT url = '{$url}' FROM slimlink ";
-      // Get resource - Collection of DB Rows
-      $result = mysqli_query($connection, $query);
-      // Test if there was a query error, not if the query was empty.
-      
-      if (!$result) {
-        die("Database query failed SELECT: " . mysqli_error($connection));
-      }
-
-      diag_echo('From $result:');
-      result_diag_echo($result);
-      echo '-----';
-      echo '<br />';
-      echo mysqli_field_count($result);
-      echo '<br />';
-      echo '-----';
-
-    } else {
-        $error_message = "Invalid URL.";
-    }
-
+  function exists($result) {
+    return empty($result);
   }
 
   function diag_echo($string) {
@@ -64,8 +14,6 @@
       echo "<hr />";
     }
   }
-
-  diag_echo($_POST["url"]);
 
   function get_true_or_false($bool) {
     if ($bool === True) {
@@ -93,6 +41,74 @@
       return $new_url;
     }
   }
+
+  function is_valid_url($url) {
+    return (filter_var($url, FILTER_VALIDATE_URL) !== false);
+  }
+
+  $error_message = '';
+  $success_message = '';
+  $information_message = '';
+
+  $dbhost = "localhost";
+  $dbuser = "chris_php";
+  $dbpass = "bagel";
+  $dbname = "php_chris";
+  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+  if(mysqli_connect_error()) {
+    die(
+        mysqli_connect_error() . " (" .
+        mysqli_connect_errno() . ")"
+       );
+      }
+  // Logic to check against POST, valid POST, generate 6 character string.
+  // Validate url
+  // Check for 'http' in url
+  // Remove http:// or https:// from url for generic DB save
+  // Check to see if the URL already exists in the DB
+
+  if (isset($_POST["url"])) {
+    print_r($_POST); echo '<br />';
+    $url = $_POST["url"];
+    $url = add_http_url($url);
+    if (is_valid_url($url) === True) {
+      $url = remove_http_url($url);
+      $query = "SELECT url = '{$url}' FROM slimlink ";
+      // Get resource - Collection of DB Rows
+      $result = mysqli_query($connection, $query);
+      // Test if there was a query error, but does not test if the query was empty.
+      
+      if (!$result) {
+        die("Database query failed SELECT: " . mysqli_error($connection));
+      }
+      
+      diag_echo('From $result:');
+      result_diag_echo($result);
+      echo '-----';
+      echo '<br />';
+      $url_exists = exists($result);
+      echo get_true_or_false($url_exists);
+      echo '<br />';
+      echo '-----';
+
+      if ($url_exists === False) {
+        $query = "INSERT INTO slimlink (trimmed_url, url) ";
+        $query .= "VALUES ('{$trimmed_url}', {$url})";
+        $insert_result = mysqli_query($connection, $query);
+        // Test if there was a query error.
+        if (!$insert_result) {
+          die("Database query failed INSERT: " . mysqli_error($connection));
+        }
+      }
+
+    } else {
+        $error_message = "Invalid URL.";
+    }
+
+  }
+
+  diag_echo($_POST["url"]);
 
   // // If the user sent a valid url via POST, generate a 
   // // trimmed_url and insert it into the DB along with
