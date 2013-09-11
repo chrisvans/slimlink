edc3e50b43c426bb3ae74b98e4ec31288dc0a07e
chrisvans <chrisvanschyndel@gmail.com>
2013-09-11 14:52:10 -0400

    Refactored string randomization, functions, logic

---
+++ slimlink/includes.php
@@ -0,0 +1,133 @@
+<?php
+
+  function get_array_from_result($result) {
+    $db_data = array();
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($db_data, $row);
+    }
+
+    return $db_data;
+  }
+
+
+  function generate_random_string($length = 6) {
+      $characters = 'abcdefghjkmnopqrstuvwxyzABCDEFGHJKMNOPQRSTUVWXYZ';
+      $random_string = '';
+      for ($i = 0; $i < $length; $i++) {
+        $random_string .= $characters[rand(0, strlen($characters) - 1)];
+      }
+    return $random_string;
+  }
+
+
+  function generate_unique_trimmed_url() {
+    global $connection;
+    $trimmed_url_exists = True;
+
+    while ($trimmed_url_exists === True) {
+      $random = generate_random_string();
+      $random = mysqli_real_escape_string($connection, $random);
+      $test_query = "SELECT * FROM slimlink ";
+      $test_result = mysqli_query($connection, $test_query);
+
+      if (!$test_result) {
+        die("Database query failed SELECT: " . mysqli_error($connection));
+      }
+      
+      $db_data = get_array_from_result($test_result);
+      
+      $exist_check = False;
+      
+      foreach($db_data as $row) {
+
+        if ($row["trimmed_url"] === $random) {
+            $exist_check = True;
+        }
+
+      }
+      
+      if (!($exist_check === True)) {
+        $trimmed_url_exists = False;
+      }
+
+    }
+
+    return $random;
+  }
+
+
+  function url_exists_in_db($db_data, $url) {
+
+    foreach($db_data as $row) {
+
+        if ($row["url"] === $url) {
+            global $trimmed_url;
+            $trimmed_url = $row["trimmed_url"];
+            return True;
+        }
+
+    }
+    
+    return False;
+  }
+
+
+  function diag_echo($string) {
+    echo '<br />' . $string . '<br />';
+  }
+
+
+  function result_diag_echo($result) {
+    $diag_result = $result;
+
+    while($row = mysqli_fetch_assoc($diag_result)) {
+      var_dump($row);
+      echo "<hr />";
+    }
+
+  }
+
+
+  function get_true_or_false($bool) {
+
+    if ($bool === True) {
+        return 'True';
+    } else {
+        return 'False';
+    }
+
+  }
+
+
+  function add_http_url($url) {
+
+    if (strpos($url, "http://") !== True) {
+      $new_url = str_replace($url, "http://" . $url, $url);
+      return $new_url;
+    } else {
+      return $url;
+    }
+
+  }
+
+
+  function remove_http_url($url) {
+
+    if (strpos($url, "http://") !== False) {
+      $new_url = str_replace("http://", "", $url);
+      return $new_url;
+    } elseif (strpos($url, "https://") !== False) {
+      $new_url = str_replace("https://", "", $url);
+      return $new_url;
+    }
+
+  }
+
+
+  function is_valid_url($url) {
+    return (filter_var($url, FILTER_VALIDATE_URL) !== false);
+  }
+
+
+?>
--- slimlink/index.php
+++ slimlink/index.php
@@ -1,122 +1,6 @@
-<?php
-
-
-  function get_array_from_result($result) {
-    $db_data = array();
-    
-    while ($row = mysqli_fetch_assoc($result)) {
-        array_push($db_data, $row);
-    }
-
-    return $db_data;
-  }
-
-
-  function generate_unique_trimmed_url() {
-    global $connection;
-    $trimmed_url_exists = True;
-
-    while ($trimmed_url_exists === True) {
-      $random = substr(md5(microtime()),rand(0,26),6);
-      $random = mysqli_real_escape_string($connection, $random);
-      $test_query = "SELECT * FROM slimlink ";
-      $test_result = mysqli_query($connection, $test_query);
-
-      if (!$test_result) {
-        die("Database query failed SELECT: " . mysqli_error($connection));
-      }
-      
-      $db_data = get_array_from_result($test_result);
-      
-      $exist_check = False;
-      
-      foreach($db_data as $row) {
-
-        if ($row["trimmed_url"] === $random) {
-            $exist_check = True;
-        }
-
-      }
-      
-      if (!($exist_check === True)) {
-        $trimmed_url_exists = False;
-      }
-
-    }
-
-    return $random;
-  }
-
-
-  function url_exists_in_db($db_data, $url) {
-
-    foreach($db_data as $row) {
-
-        if ($row["url"] === $url) {
-            return True;
-        }
-
-    }
-    
-    return False;
-  }
-
-
-  function diag_echo($string) {
-    echo '<br />' . $string . '<br />';
-  }
-
-
-  function result_diag_echo($result) {
-    $diag_result = $result;
-
-    while($row = mysqli_fetch_assoc($diag_result)) {
-      var_dump($row);
-      echo "<hr />";
-    }
-
-  }
-
-
-  function get_true_or_false($bool) {
-
-    if ($bool === True) {
-        return 'True';
-    } else {
-        return 'False';
-    }
-
-  }
-
-
-  function add_http_url($url) {
-
-    if (strpos($url, "http://") !== True) {
-      $new_url = str_replace($url, "http://" . $url, $url);
-      return $new_url;
-    } else {
-      return $url;
-    }
-
-  }
-
-
-  function remove_http_url($url) {
-
-    if (strpos($url, "http://") !== False) {
-      $new_url = str_replace("http://", "", $url);
-      return $new_url;
-    } elseif (strpos($url, "https://") !== False) {
-      $new_url = str_replace("https://", "", $url);
-      return $new_url;
-    }
-
-  }
-
-
-  function is_valid_url($url) {
-    return (filter_var($url, FILTER_VALIDATE_URL) !== false);
-  }
+<?php 
+  
+  require("includes.php");
 
 
   $error_message = '';
@@ -137,7 +21,6 @@
   }
 
   if (isset($_POST["url"])) {
-    print_r($_POST); echo '<br />';
     $url = $_POST["url"];
     $url = add_http_url($url);
 
@@ -155,12 +38,9 @@
         
       $db_data = get_array_from_result($result);
       $url_exists = url_exists_in_db($db_data, $url);
-      diag_echo(get_true_or_false($url_exists));
-      $random = generate_unique_trimmed_url();
-      diag_echo($random);
 
       if ($url_exists === False) {
-        $trimmed_url = $random;
+        $trimmed_url = generate_unique_trimmed_url();
         $url = $url;
         $query = "INSERT INTO slimlink (trimmed_url, url) ";
         $query .= "VALUES ('{$trimmed_url}', '{$url}')";
@@ -172,13 +52,14 @@
         }
 
       }
+      $success_message = "{$url} successfully trimmed!";
+      $information_message = "Access your URL at www.slimlink.com/{$trimmed_url}";
+      
     } else {
         $error_message = "Invalid URL.";
     }
 
   }
-
-  diag_echo($_POST["url"]);
 
 ?>
 <html>
